547d0b1abc202be41ee27aa43e1abc20d063924d
mentasuave01 <mentasuave01@gmail.com>
2025-11-22 18:22:29 +0100

    ✨ Add new video file bleeth-02.mp4 and update video source in page.tsx

--- bleeth-frontend/src/app/page.tsx
+++ bleeth-frontend/src/app/page.tsx
@@ -3,18 +3,18 @@ export default function Home() {
     <div className="flex flex-col h-screen items-center justify-center bg-black overflow-hidden">
       
       <div className="flex items-center justify-center w-full h-full">
-        <img src="/frame.avif" alt="Bleeth" className="w-32 h-full " />
-        <div className="w-full h-full bg-black/50 flex items-center justify-center flex-col">
+        <img src="/frame.avif" alt="Bleeth" className="w-20 h-full " />
+        <div className="max-w-96 w-full h-full bg-black/50 flex items-center  flex-col">
         <video
         autoPlay
         muted
         playsInline
         className="max-w-[90vw] max-h-[200px] w-auto h-auto"
       >
-        <source src="/bleeth.mp4" type="video/mp4" />
+        <source src="/bleeth-02.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video></div>
-        <img src="/frame.avif" alt="Bleeth" className="w-32 h-full rotate-180" />
+        <img src="/frame.avif" alt="Bleeth" className="w-20 h-full rotate-180" />
       </div>
     </div>
   );
