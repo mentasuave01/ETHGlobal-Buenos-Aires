d03636464c22e08f04dc201b9c66b21638fb06d6
Draiakoo <polureher@gmail.com>
2025-11-23 07:22:53 -0300

    started deployment script

--- contracts/scripts/send-op-tx.ts
+++ contracts/scripts/send-op-tx.ts
@@ -6,26 +6,20 @@ const { viem } = await network.connect({
 });
 
 console.log("Sending transaction using the OP chain type");
+viem.sendDeploymentTransaction
 
 const publicClient = await viem.getPublicClient();
 const [senderClient] = await viem.getWalletClients();
 
 console.log("Sending 1 wei from", senderClient.account.address, "to itself");
 
-const l1Gas = await publicClient.estimateL1Gas({
-  account: senderClient.account.address,
-  to: senderClient.account.address,
-  value: 1n,
-});
-
-console.log("Estimated L1 gas:", l1Gas);
-
-console.log("Sending L2 transaction");
-const tx = await senderClient.sendTransaction({
-  to: senderClient.account.address,
-  value: 1n,
-});
-
-await publicClient.waitForTransactionReceipt({ hash: tx });
-
-console.log("Transaction sent successfully");
+const owner = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266';
+// const hash = await senderClient.deployContract({
+//   // abi: wagmiAbi,
+//   // bytecode: ,
+//   args: [
+//     owner,
+//     '0x6e7d74fa7d5c90fef9f0512987605a6d546181bb',   // pyth entropy
+//     '0x8250f4aF4B972684F7b336503E2D6dFeDeB1487a'    // pyth price feeds
+//   ]
+// })
